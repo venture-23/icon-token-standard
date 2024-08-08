cc76e537edb1cbd0ec7a027448c90c03e94e0a63
simusud <simkhadasudeep9@gmail.com>
2024-08-08 09:28:39 +0545

    Add mint fn and deploy plugin for impl example

--- java-contracts/token-examples/NewCrossTokenDeploy/src/main/java/icon/cross/chain/token/examples/NewCrossTokenImpl.java
+++ java-contracts/token-examples/NewCrossTokenDeploy/src/main/java/icon/cross/chain/token/examples/NewCrossTokenImpl.java
@@ -1,14 +1,24 @@
 package icon.cross.chain.token.examples;
 
 
+import foundation.icon.xcall.NetworkAddress;
 import icon.cross.chain.token.lib.tokens.HubTokenImpl;
 import score.Address;
+import score.annotation.External;
 
 import java.math.BigInteger;
 
+import static icon.cross.chain.token.lib.utils.Check.onlyOwner;
 
-public class NewCrossTokenImpl extends HubTokenImpl{
-    public NewCrossTokenImpl(Address _xCall, String[] sources, String[] destinations, String _nid, String _tokenName, String _symbolName, BigInteger _decimals) {
-        super(_xCall, sources, destinations, _nid, _tokenName, _symbolName, _decimals);
+
+public class NewCrossTokenImpl extends HubTokenImpl {
+    public NewCrossTokenImpl(Address _xCall, String _nid, String _tokenName, String _symbolName, BigInteger _decimals) {
+        super(_xCall, _nid, _tokenName, _symbolName, _decimals);
+    }
+
+    @External
+    public void mint(String to, BigInteger amount) {
+        onlyOwner();
+        super._mint(NetworkAddress.parse(to), amount);
     }
 }
