5f0777d03e263c642c7ac4ba90276ee955e9d8f7
Sandip801 <sandip.bhujel@ibriz.ai>
2024-08-15 11:59:55 +0545

    updated script

--- solidity-contracts/icon-cross-chain-token/script/DeployCrossToken.s.sol
+++ solidity-contracts/icon-cross-chain-token/script/DeployCrossToken.s.sol
@@ -3,14 +3,17 @@ import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
 import "@multi-token-standard/implementation/NewCrossToken.sol";
 import "openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@multi-token-standard/tokens/SpokeTokenManager.sol";
 
 contract DeployCrossToken is Script {
 
     using Strings for string;
 
     uint256 internal deployerPrivateKey;
+    SpokeTokenManager spokeTokenManager;
+    address public spokeTokenManagerAddress;
 
-    address xCallManager = 0xd5CECE180a52e0353654B3337c985E8d5E056344;
+
     constructor() {
         deployerPrivateKey = vm.envUint("PRIVATE_KEY");
     }
@@ -21,7 +24,7 @@ contract DeployCrossToken is Script {
         vm.stopBroadcast();
     }
 
-    function deployContract() public broadcast(deployerPrivateKey) {
+    function deployNewCrossTokenContract(string memory name, string memory symbol, address xCall, address iconTokenAddress, address xCallManager) public broadcast(deployerPrivateKey) {
         NewCrossToken crossToken = new NewCrossToken(); 
         NewCrossToken newCrossToken = NewCrossToken(
             address(
@@ -29,16 +32,37 @@ contract DeployCrossToken is Script {
                     address(crossToken),
                     abi.encodeWithSelector(
                         crossToken.initialize.selector,
-                        "NewCrossToken", 
-                        "NCT",
-                        0x28ecb198e86a7FcA1cf51032635967fc26cDDAaD,
-                        "0x2.icon/cx38cfd5689c7951606d049c04b0a4a549c2910b6b",
+                        name,
+                        symbol,
+                        xCall,
+                        iconTokenAddress,
                         xCallManager
                     )
                 )
             )
         );
         console2.log(address(newCrossToken));
+    }
+
+    function deploySpokeTokenManager(address token, address xCall, address iconTokenAddress, address xCallManager) public broadcast(deployerPrivateKey) {
+        spokeTokenManager = new SpokeTokenManager();
+        spokeTokenManagerAddress = address(spokeTokenManager);
+        spokeTokenManager = SpokeTokenManager(
+            address(
+                new ERC1967Proxy(
+                    spokeTokenManagerAddress,
+                    abi.encodeWithSelector(
+                        spokeTokenManager.initialize.selector,
+                        address(token), 
+                        address(xCall),
+                        iconTokenAddress,
+                        address(xCallManager)
+                    )
+                )
+            )
+        );
+        console2.log("SpokeTokenManager (Implementation) address:", spokeTokenManagerAddress);
+        console2.log("SpokeTokenManager (Proxy) address:", address(spokeTokenManager));
     }
 
 }
